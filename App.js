b3fc1aca88a83486040b6c21717a228083d8bf2d
v8-fly <98163507+v8-fly@users.noreply.github.com>
2023-01-09 11:34:14 +0530

    React Elements setUp

--- App.js
+++ App.js
@@ -2,29 +2,49 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "heading123",
-    key: 1,
-  },
-  "DIL SE REACT ♥️♥️♥️"
-)
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "heading123",
-    key: 2,
-  },
-  "SUPER STUFF 💫💫💫"
+// const heading1 = React.createElement(
+//   "h1",
+//   {
+//     id: "heading123",
+//     key: 1,
+//   },
+//   "DIL SE REACT ♥️♥️♥️"
+// )
+
+const Heading1 = (
+  <h1 id="heading123" key={1}>
+    DIL SE REACT ♥️♥️♥️
+  </h1>
 )
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-    key: 3,
-  },
-  [heading1, heading2]
+// const heading2 = React.createElement(
+//   "h1",
+//   {
+//     id: "heading123",
+//     key: 2,
+//   },
+//   "SUPER STUFF 💫💫💫"
+// )
+
+const Heading2 = (
+  <h2 id="heading123" key={2}>
+    SUPER STUFF 💫💫💫
+  </h2>
 )
-root.render(container)
+
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//     key: 3,
+//   },
+//   [heading1, heading2]
+// )
+
+const Container = (
+  <div>
+    {Heading1}
+    {Heading2}
+  </div>
+)
+root.render(Container)
